93f1a2866d0b00a0951080ca7b8ea63f08cba9b3
saltchicken <john.eicher89@gmail.com>
2023-06-12 20:17:17 -0700

    Initial commit

---
+++ grpc_client.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import logging
+
+import grpc
+import proto_pb2
+import proto_pb2_grpc
+
+
+def run():
+    # with grpc.insecure_channel('192.168.1.45:50051') as channel:
+    with grpc.insecure_channel('127.0.0.1:50051') as channel:
+        stub = proto_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(proto_pb2.HelloRequest(name='you'))
+        print(response.message)
+        response = stub.SayHelloAgain(proto_pb2.HelloRequest(name='you'))
+        print("Greeter client received: " + response.message)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
