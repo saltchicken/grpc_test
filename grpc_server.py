fbd676fb62fb5548bb3440081373821e8f511bce
saltchicken <john.eicher89@gmail.com>
2023-06-13 19:17:36 -0700

    Add SSL

--- grpc_server.py
+++ grpc_server.py
@@ -21,7 +21,17 @@ def serve():
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     proto_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:' + port)
+    with open('../server.crt', 'rb') as f:
+        server_crt_data = f.read()
+    with open('../server.key', 'rb') as f:
+        server_key_data = f.read()
+    credentials = grpc.ssl_server_credentials(
+        [(server_key_data, server_crt_data)],
+        root_certificates=None,
+        require_client_auth=True  # Enable client authentication
+    )
+    # server.add_insecure_port('[::]:' + port)
+    server.add_secure_port('[::]:' + port, credentials)
     server.start()
     print("Server started, listening on " + port)
     server.wait_for_termination()
